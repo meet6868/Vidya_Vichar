3507697b1f91edc7b6e838339a3f1034d7f6b7e1
Ghelani Meet Nileshbhai <ghelanimeet2002@gmail.com>
2025-10-03 11:04:01 +0530

    For deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,"10.42.0.33", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
